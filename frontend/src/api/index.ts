56276f0aff1dcc852f36a0f2c697f2c370321c63
Bruno França <bsfranca2@gmail.com>
2023-09-05 14:49:17 -0300

    feat(front) api url from env

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { PrinterMetrics, PrinterAgent } from "./types";
 
 const api = axios.create({
-  baseURL: "http://localhost:5091/",
+  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:5091/",
 });
 
 export const getPrinterMetrics = () => api.get<PrinterMetrics>("/printer-metrics").then((res) => res.data);
