bb785221f5192388a76de59b4b2db51567d664fc
Bruno França <bsfranca2@gmail.com>
2023-09-06 12:00:02 -0300

    feat(api) create prescription

---
+++ Opuspac.Api/Controllers/PrescriptionController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Opuspac.Api.Models.Request;
+using Opuspac.Core.Repositories;
+
+namespace Opuspac.Api.Controllers;
+
+[ApiController]
+[Route("/prescriptions")]
+public class PrescriptionController : Controller
+{
+    private IPrescriptionRepository _prescriptionRepository { get; set; }
+    private IPrescriptionMedicineRepository _prescriptionMedicineRepository { get; set; }
+
+    public PrescriptionController(
+        IPrescriptionRepository prescriptionRepository,
+        IPrescriptionMedicineRepository prescriptionMedicineRepository)
+    {
+        _prescriptionRepository = prescriptionRepository;
+        _prescriptionMedicineRepository = prescriptionMedicineRepository;
+    }
+
+    [HttpPost]
+    public async Task<IResult> Post([FromBody] PrescriptionRequestModel prescriptionRequestModel)
+    {
+        var prescription = prescriptionRequestModel.ToPrescription();
+        await _prescriptionRepository.CreateAsync(prescription);
+        var prescriptionMedicines = prescriptionRequestModel.Medicines.Select(p => p.ToPrescriptionMedicine(prescription.Id)).ToList();
+        foreach (var prescriptionMedicine in prescriptionMedicines)
+        {
+            await _prescriptionMedicineRepository.CreateAsync(prescriptionMedicine);
+        }
+        return Results.Ok(prescription);
+    }
+}
---
+++ Opuspac.Api/Models/Request/PrescriptionRequestModel.cs
@@ -0,0 +1,41 @@
+﻿using Opuspac.Core.Entities;
+
+namespace Opuspac.Api.Models.Request;
+
+public class PrescriptionRequestModel
+{
+    public Guid PatientId { get; set; }
+
+    public string Code { get; set; }
+
+    public string AttendantId { get; set; }
+
+    public string Time { get; set; }
+
+    public List<PrescriptionMedicineRequestModel> Medicines { get; set; }
+
+    public Prescription ToPrescription()
+    {
+        return new Prescription
+        {
+            PatientId = PatientId,
+            Code = Code,
+            AttendantId = AttendantId,
+            Time = Time
+        };
+    }
+}
+
+public record PrescriptionMedicineRequestModel(string Name, int Quantity, string AdministrationInstructions)
+{
+    public PrescriptionMedicine ToPrescriptionMedicine(Guid prescriptionId)
+    {
+        return new PrescriptionMedicine
+        {
+            PrescriptionId = prescriptionId,
+            MedicineName = Name,
+            Quantity = Quantity,
+            AdministrationInstructions = AdministrationInstructions,
+        };
+    }
+}
